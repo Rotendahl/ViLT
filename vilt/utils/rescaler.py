572c9bdc52d41ca3fc220627fb81e127e96731fd
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2021-12-08 15:28:10 +0100

    Added rescaler

--- vilt/utils/rescaler.py
+++ vilt/utils/rescaler.py
@@ -1,10 +1,16 @@
 from PIL import Image
 
 from vilt.transforms.pixelbert import pixelbert_transform
+import torch
+import io
+import pickle
 
 IMAGE_SIZE = 384
 
 
 def load_and_rescale(path):
     img = Image.open(path).convert("RGB")
-    return pixelbert_transform(IMAGE_SIZE)(img).tolist()
+    buffer = io.BytesIO()
+    img = pixelbert_transform(IMAGE_SIZE)(img)
+    torch.save(img, buffer)
+    return pickle.dumps(img)
